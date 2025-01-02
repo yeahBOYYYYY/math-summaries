4760b1e33c7aa97cfe7fd7e2d80face2a11eb412
yeahBOYYYYY <mikhelman.ron@gmail.com>
2025-01-02 03:07:24 +0200

    Commit - 02/01/2025 03:07:24

--- lbl/autocolorlyx.py
+++ lbl/autocolorlyx.py
@@ -96,9 +96,9 @@ if __name__ == "__main__":
 
     # colorAbsolute(path, color, from_line, to_line)
 
-    # absulote_path = r"E:\University\!Summaries\lbl\\"
+    absulote_path = r"E:\University\!Summaries\lbl\\"
     # absulote_path = r"C:\Users\user\OneDrive\Documents\summaries\lbl\\"
-    absulote_path = r"./lbl/"
+    # absulote_path = r"./lbl/"
 
-    path = "group_theory_lbl.lyx"
+    path = "set_theory_lbl.lyx"
     colorChanges(absulote_path + path)
