7adb525b8d65446b9625f2167eb6daed37cf9d6e
yeahBOYYYYY <mikhelman.ron@gmail.com>
2025-02-22 20:44:23 +0200

    Commit - 22/02/2025 20:44:23

--- website/main.js
+++ website/main.js
@@ -60,18 +60,16 @@ async function createButtons(){
     const jsonRes = await parseJsonOFCourses();
     for (var i = 0; i < jsonRes.length; i++){
         const pdf_name = jsonRes[i][0];
-        const { display_name, summary_type, button_color, course_info } = jsonRes[i][1];
+        const { display_name, summary_type, button_color, course_info, repair_needed } = jsonRes[i][1];
         let courseInfo = await getCourseInfo(course_info);
-        setButtonCode(pdf_name, display_name, summary_type, button_color, courseInfo);
+        setButtonCode(pdf_name, display_name, summary_type, button_color, courseInfo, repair_needed);
     }
 }
 
 async function parseJsonOFCourses() {
     try {
         const response = await fetch('./website/pdf_data.json');
-        if (!response.ok) {
-            throw new Error(`Failed to load JSON: ${response.statusText}`);
-        }
+        if (!response.ok) throw new Error(`Failed to load JSON: ${response.statusText}`);
         const pdfData = await response.json();
         return Object.entries(pdfData);
     } catch (error) {
@@ -84,36 +82,60 @@ async function getCourseInfo(infoPath) {
     let wholePath = COURSES_INFO_PATH + "/" + infoPath;
 
     try {
-        let response = await fetch(wholePath);
-        let data = await response.text();
-        let formattedData = data.replace(/\n/g, '<br>');
-        
-        return formattedData;
+        const response = await fetch(wholePath);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.text();
+        return data.replace(/\n/g, '<br>');
     } catch (error) {
+        console.error('Error fetching info:', error);
         return "Error";
     }
 }
 
-function setButtonCode(pdfName, buttonText, summaryType, buttonColor, courseInfo) {
+function setButtonCode(pdfName, buttonText, summaryType, buttonColor, courseInfo, neededRepair) {
     try {
         const elem = document.querySelector(`[title="${pdfName}"]`);
-        // const divTitle = elem.getAttribute('title');
-        const newElement = document.createElement('div');
+
+        let pdfLink;
         if (summaryType === "lbl"){
-            newElement.innerHTML = getButtonCode(buttonText, `${LBL_SUMM_PATH}/${pdfName}.pdf`, buttonColor, courseInfo);
+            pdfLink = `${LBL_SUMM_PATH}/${pdfName}.pdf`;
         } else if (summaryType === "other"){
-            newElement.innerHTML = getButtonCode(buttonText, `${OTHER_SUMM_PATH}/${pdfName}.pdf`, buttonColor, courseInfo);
+            pdfLink = `${OTHER_SUMM_PATH}/${pdfName}.pdf`;
         } else if (summaryType === "cheatsheet"){
-            newElement.innerHTML = getButtonCode(buttonText, `${CHEATSHEET_SUMM_PATH}/${pdfName}.pdf`, buttonColor, courseInfo);
+            pdfLink = `${CHEATSHEET_SUMM_PATH}/${pdfName}.pdf`;
         } else {
             console.warn('Button request is from unkown summery class:', pdfName);
             return;
         }
+
+        const newElement = document.createElement('div');
+
+        if (neededRepair == null) {
+            newElement.innerHTML = getButtonCode(buttonText, pdfLink, buttonColor, courseInfo);
+        } else {
+            newElement.innerHTML = getRepairButtonCode(buttonText, pdfLink, buttonColor, courseInfo, neededRepair);
+        }
+
         elem.replaceWith(newElement.firstElementChild);
-        return;
     } catch (error) {
         console.warn('Not found a button request for:', pdfName);
     }
+}
+
+function getRepairButtonCode(buttonText, pdfLink, gradient, courseInfo, neededRepair) {
+    return `
+        <div class="pdf-button-container ${gradient}">
+            <a class="pdf-button" href="${pdfLink}" target="_blank">${buttonText}</a>
+            <div class="tooltip">
+                <img src="website/repair.png">
+                <span class="tooltiptext">${neededRepair}</span>
+            </div>
+            <div class="tooltip">
+                <img src="${INFO_IMG_PATH}">
+                <span class="tooltiptext">${courseInfo}</span>
+            </div>
+        </div>
+    `;
 }
 
 function getButtonCode(buttonText, pdfLink, gradient, courseInfo) {
@@ -121,8 +143,8 @@ function getButtonCode(buttonText, pdfLink, gradient, courseInfo) {
         <div class="pdf-button-container ${gradient}">
             <a class="pdf-button" href="${pdfLink}" target="_blank">${buttonText}</a>
             <div class="tooltip">
-            <img src="${INFO_IMG_PATH}">
-            <span class="tooltiptext">${courseInfo}</span>
+                <img src="${INFO_IMG_PATH}">
+                <span class="tooltiptext">${courseInfo}</span>
             </div>
         </div>
     `;
