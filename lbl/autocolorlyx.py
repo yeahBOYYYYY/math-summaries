e83a1b452062f0c3a86799b6000fb1b60f954745
yeahBOYYYYY <mikhelman.ron@gmail.com>
2025-03-23 17:34:19 +0200

    Commit - 23/03/2025 17:34:19

--- lbl/autocolorlyx.py
+++ lbl/autocolorlyx.py
@@ -100,5 +100,5 @@ if __name__ == "__main__":
     # absulote_path = r"C:\Users\user\OneDrive\Documents\summaries\lbl\\"
     # absulote_path = r"./lbl/"
 
-    path = "number_theory_lbl.lyx"
+    path = "complex_functions2_lbl.lyx"
     colorChanges(absulote_path + path)
